ab74d35e48db13ec65e7619d616eb208054e6244
Matheus Guilherme Sousa Bezerra <84602548+mazklau@users.noreply.github.com>
2024-08-29 17:42:49 -0300

    Update

--- Dashboard-js/scrip.js
+++ Dashboard-js/scrip.js
@@ -2,9 +2,13 @@ const express = require('express');
 const cors = require('cors');
 const { fetchData } = require('./script.js'); // Importa a função para buscar dados do banco de dados
 
+const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
 // Configura o CORS para permitir solicitações de qualquer origem
 app.use(cors());
 
